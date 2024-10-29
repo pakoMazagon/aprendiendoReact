f98df6a68918deb0184063ed60a904ec8f6cfa2e
FRC <pako.rc10.4@gmail.com>
2024-10-29 23:14:10 +0100

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,93 @@
-import { useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import gruñonImg from './assets/images/Gruñon.png'
 import filosofoImg from './assets/images/Filosofo.png'
 import fortachonImg from './assets/images/Fortachon.png'
-import incognitImg from './assets/images/incognit.png'
+import azraelImg from './assets/images/Azrael.png'
+import Smurf from './components/Smurf.tsx'
+import { v4 as uuidv4 } from 'uuid'
 import './App.css'
 
-interface SmurfProps {
-  name: string;
-  image: string;
-}
-
-const Smurf: React.FC<SmurfProps> = ({name, image}) => {
-  return (
-    <div>
-      <h2>{name}</h2>
-      <img src={image} alt={name} />
-    </div>
-  );
+function shuffleArray(array: any[]): any[] {
+  return array
+    .map(value => ({ ...value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ sort, ...value }) => value);
 }
 
 
 
 function App() {  
-  const smurfs:Object[] = [{name:'Gruñon',img:gruñonImg},{name:'Fortachon',img:fortachonImg}, {name:'Filosofo',img:filosofoImg}];
+  const initialSmurfs:any[] = 
+            [
+              {id:uuidv4(),name:'Gruñon',image:gruñonImg, isEnable:false, color:'red'},
+              {id:uuidv4(),name:'Fortachon',image:fortachonImg, isEnable:false,color:'yellow'}, 
+              {id:uuidv4(),name:'Filosofo',image:filosofoImg, isEnable:false,color:'green'}, 
+              {id:uuidv4(),name:'Azrael',image:azraelImg, isEnable:false,color:'blue'}];
 
-  const [smurfIndex, setSmurfIndex] = useState(0);
+  // Duplicamos las cartas para crear las parejas y luego barajamos
+  const [cards, setCards] = useState(() =>
+    shuffleArray([...initialSmurfs, ...initialSmurfs.map(card => ({ ...card, id: uuidv4() }))])
+  );
 
-  const refSmurf = useRef<any>(smurfs[0].name);  
+  const [selectedCards, setSelectedCards] = useState<any>([]);
 
-  const changeSmurfImage = (e:any) =>{        
-    if(e.target.src.includes(incognitImg)){      
-      e.target.style.visibility="hidden";
-      e.target.src = "";
+  const handleCardClick = (id:any) => {
+    console.log('handleCardClick fuera:'+id+',selectedCards:'+selectedCards);
+    if (selectedCards.length < 2) {
+      setSelectedCards([...selectedCards, id]);
+      setCards(cards =>
+        cards.map(card =>
+          card.id === id? { ...card, isEnable: true } : card
+        )
+      );
+      console.log('handleCardClick dentro:'+selectedCards);
     }
-    else{
-      e.target.src = incognitImg;
-      console.log(`e.target parentNode es`+e.target.parentNode)      
-    }    
-    e.target.parentNode.style.background = "white";
-  }
+  };
 
-  const changeSmurfText = (e:any) =>{    
-    console.log(`e.target es`+e.target.innerHTML)
-    if(e.target.innerHTML == "Visto"){
-      e.target.style.visibility="hidden";
-      e.target.innerHTML = "";
+  useEffect(() => {
+    console.log('useEffect fuera'+selectedCards);
+    if (selectedCards.length === 2) {
+      console.log('useEffect dentro:'+selectedCards);
+      const [first, second] = selectedCards;
+      const firstCard = cards.find(card => card.id === first);
+      const secondCard = cards.find(card => card.id === second);
+
+      if (firstCard && secondCard && firstCard.name === secondCard.name) {
+        // Las cartas coinciden, mantén reveladas ambas
+        console.log('Las cartas coinciden, mantén reveladas ambas');
+        setCards(cards =>
+          cards.map(card =>
+            card.id === first || card.id === second ? { ...card, isEnable: true } : card
+          )
+        );
+      } else {
+        console.log('Las cartas no coinciden, ocúltalas nuevamente después de un retraso')
+        // Las cartas no coinciden, ocúltalas nuevamente después de un retraso
+        setTimeout(() => {
+          setCards(cards =>
+            cards.map(card =>
+              card.id === first || card.id === second ? { ...card, isEnable: false } : card
+            )
+          );
+        }, 1000);
+      }
+      // Limpiamos la selección después de comparar
+      setSelectedCards([]);
     }
-    else{
-      e.target.innerHTML = "Visto";
-    }    
-  }
+  }, [selectedCards, cards]);
+  
+
+  
+
+  
 
   return (
     <>                      
-      <h1>Los Pitufos</h1>       
+      <h1>Los Pitufos</h1>
       <div className='principalDiv'>  
-        <div ref={refSmurf} className='smurfDiv' >         
-          <h2 onClick={changeSmurfText}>Fortachon</h2>
-          <img onClick={changeSmurfImage} src={fortachonImg} /> 
-        </div>
-        <div ref={refSmurf} className='smurfDiv' >          
-          <h2 onClick={changeSmurfText}>Filosofo</h2>
-          <img src={filosofoImg} onClick={changeSmurfImage}/>
-        </div>
-        <div ref={refSmurf} className='smurfDiv' >          
-          <h2 onClick={changeSmurfText}>"Gruñon"</h2>
-          <img src={gruñonImg} onClick={changeSmurfImage}/>
-        </div>
+        {cards.map(card =>{
+          return <Smurf key={uuidv4()} id={card.id} name={card.name} image={card.image} color={card.color} isEnable={card.isEnable} onClick={()=>handleCardClick(card.id)}/>
+        })}
       </div>
 
     </>
